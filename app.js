0b8da0c98532e91b55de2635792f167c210a9a92
jaafari-R <jaafariramadan@gmail.com>
2023-12-17 12:05:35 +0200

    init server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+})
