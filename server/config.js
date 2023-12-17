e893645d0328d8e69bc5fef76673f94364470569
jaafari-R <jaafariramadan@gmail.com>
2023-12-17 12:31:11 +0200

    Initial recipe finder api manager done

---
+++ server/config.js
@@ -0,0 +1,7 @@
+const externalApiConfigs = {
+    RECIPE_API: "https://recipes-goodness-elevation.herokuapp.com/recipes/ingredient/"
+}
+
+module.exports = {
+    externalApiConfigs
+}
--- server/external_api/recipe.js
+++ server/external_api/recipe.js
@@ -1,7 +1,21 @@
+const { default: axios } = require("axios");
+
+const { RECIPE_API } = require("../config").externalApiConfigs;
+
 class Recipe {
+    constructor() {
 
+    }
 }
 
-class RecipeAPIManager {
-
+class RecipeFinderAPIManager {
+    async getRecipesThatHaveIngredient(ingredient) {
+        return await axios.get(RECIPE_API + ingredient)
+        .then((response) => {
+            return response.data.results;//.map(recipe => new Recipe(recipe));
+        });
+    }
 }
+
+const rfam = new RecipeFinderAPIManager();
+rfam.getRecipesThatHaveIngredient("tomato");
