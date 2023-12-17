287ac46619e6238dee2f76f30517908cf4279ae3
jaafari-R <jaafariramadan@gmail.com>
2023-12-17 12:16:36 +0200

    created basic app layout

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.use(express.urlencoded({extended: false}));
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
-})
+});
---
+++ server/external_api/recipe.js
@@ -0,0 +1,7 @@
+class Recipe {
+
+}
+
+class RecipeAPIManager {
+
+}
